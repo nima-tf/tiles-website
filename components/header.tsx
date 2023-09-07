be01c5257a108fdf0933d12061f04bfe9b7e8ef8
nima-tf <nima.taherifard@gmail.com>
2023-09-07 15:36:12 -0400

    added header component retry

---
+++ components/header.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import { LucideIcon } from "lucide-react";
+
+interface HeaderProps {
+    title: string,
+    description: string,
+    icon: LucideIcon,
+    iconColor?: string,
+    iconBg?: string
+}
+
+
+const Header = (
+    {
+        title,
+         description,
+        icon: Icon,
+        iconColor,
+        iconBg
+    }: HeaderProps
+    ) => { 
+        return ( 
+            <div className="border-2 border-red-500 flex px-4 lg:px-8 items-center mb-8 gap-x-3">
+                <div className={cn("w-fit p-2 rounded-md", iconBg)}>
+                    <Icon className={cn("h-8 w-8 ", iconColor)} />
+                </div>
+                <div>
+                    <h1 className="text-3xl font-bold">{title}</h1>
+                    <p className="text-sm text-muted-foreground">{description}</p>
+                </div>
+            </div>
+        );
+}
+ 
+export default Header;
