f3e116867532d97111b5bed81b9459902f912163
nima-tf <80435906+nima-tf@users.noreply.github.com>
2023-09-11 16:15:24 -0400

    replicateai api setup

--- components/header.tsx
+++ components/header.tsx
@@ -2,34 +2,31 @@ import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 
 interface HeaderProps {
-    title: string,
-    description: string,
-    icon: LucideIcon,
-    iconColor?: string,
-    iconBg?: string
+  title: string;
+  description: string;
+  icon: LucideIcon;
+  iconColor?: string;
+  iconBg?: string;
 }
 
+const Header = ({
+  title,
+  description,
+  icon: Icon,
+  iconColor,
+  iconBg,
+}: HeaderProps) => {
+  return (
+    <div className="flex px-4 lg:px-8 items-center mb-8 gap-x-3">
+      <div className={cn("w-fit p-2 rounded-md", iconBg)}>
+        <Icon className={cn("h-8 w-8 ", iconColor)} />
+      </div>
+      <div>
+        <h1 className="text-3xl font-bold">{title}</h1>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </div>
+    </div>
+  );
+};
 
-const Header = (
-    {
-        title,
-         description,
-        icon: Icon,
-        iconColor,
-        iconBg
-    }: HeaderProps
-    ) => { 
-        return ( 
-            <div className="flex px-4 lg:px-8 items-center mb-8 gap-x-3">
-                <div className={cn("w-fit p-2 rounded-md", iconBg)}>
-                    <Icon className={cn("h-8 w-8 ", iconColor)} />
-                </div>
-                <div>
-                    <h1 className="text-3xl font-bold">{title}</h1>
-                    <p className="text-sm text-muted-foreground">{description}</p>
-                </div>
-            </div>
-        );
-}
- 
 export default Header;
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -35,28 +35,22 @@ const routes = [
     color: "text-violet-500",
   },
   {
+    label: "Code Generation",
+    icon: Code,
+    href: "/code",
+    color: "text-green-700",
+  },
+  {
     label: "Image Generation",
     icon: ImageIcon,
     href: "/image",
     color: "text-pink-700",
   },
   {
-    label: "Video Generation",
-    icon: VideoIcon,
-    href: "/video",
-    color: "text-orange-700",
-  },
-  {
     label: "Music Generation",
     icon: Music,
     href: "/music",
-    color: "text-emerald-700",
-  },
-  {
-    label: "Code Generation",
-    icon: Code,
-    href: "/code",
-    color: "text-green-700",
+    color: "text-orange-700",
   },
   {
     label: "Settings",
